0d6e8069e7dadad01553014434f4c258b3f52d5a
charlyboy19 <crtf1980@gmail.com>
2023-12-28 11:39:19 -0600

    Primer repositorio

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+    }
+}
